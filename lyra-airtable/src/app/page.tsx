6b131e4ce16a04d9c83f2fa5c62989b697ebafe8
matthewdylanchin <matthewdylan@student.unimelb.edu.au>
2026-01-12 23:09:42 +1100

    made notch for table (might have to change later to render the tabs dynamically )

--- lyra-airtable/src/app/page.tsx
+++ lyra-airtable/src/app/page.tsx
@@ -1,18 +1,3 @@
-// import { CreateBaseDebug } from "./_components/CreateBaseDebug";
-// import { TableDebug } from "./_components/TableDebug";
-
-// export default function HomePage() {
-//   return (
-//     <main style={{ padding: 24 }}>
-//       <h1>Lyra Airtable Clone</h1>
-//       <CreateBaseDebug />
-//       <TableDebug />
-//     </main>
-//   );
-// }
-
-
-
 import AppTopBar from "./_components/shell/AppTopBar";
 import GridShell from "./_components/shell/GridShell";
 import LeftRail from "./_components/shell/LeftRail";
@@ -22,16 +7,11 @@ import ViewActionBar from "./_components/shell/ViewActionBar";
 export default function BasePage() {
   return (
     <div className="h-screen w-screen bg-white text-zinc-900">
-      {/* Top app bar */}
-      <AppTopBar />
-
-      {/* Table tabs bar */}
-      <TableTabsBar />
-
-      {/* View/action bar */}
-      <ViewActionBar />
-
-      {/* Body */}
+      <div className="flex flex-col">
+        <AppTopBar />
+        <TableTabsBar />
+        <ViewActionBar />
+      </div>
       <div className="flex h-[calc(100vh-44px-44px-44px)]">
         {/* Left rail */}
         <LeftRail />
