9ce9c3482217062f221f0c02ffecc274be594a6c
matthewdylanchin <matthewdylan@student.unimelb.edu.au>
2026-01-14 17:38:15 +1100

    added vertical side bar

--- lyra-airtable/src/app/base/[baseId]/layout.tsx
+++ lyra-airtable/src/app/base/[baseId]/layout.tsx
@@ -1,8 +1,9 @@
 import AppTopBar from "@/app/_components/shell/AppTopBar";
-import GridShell from "@/app/_components/shell/GridShell";
+import BaseSidebar from "@/app/_components/shell/BaseSidebar";
 import LeftRail from "@/app/_components/shell/LeftRail";
 import TableTabsBar from "@/app/_components/shell/TableTabsBar";
 import ViewActionBar from "@/app/_components/shell/ViewActionBar";
+
 
 export default function BaseLayout({
   children,
@@ -10,17 +11,22 @@ export default function BaseLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="h-screen w-screen bg-white text-zinc-900">
-      <div className="flex flex-col">
-        <AppTopBar />
-        <TableTabsBar />
-        <ViewActionBar />
-      </div>
-      <div className="flex h-[calc(100vh-44px-44px-44px)]">
-        {/* Left rail */}
-        <LeftRail />
-        {/* Grid */}
-        <div className="flex-1 overflow-auto">{children}</div>
+    <div className="h-screen w-screen overflow-hidden bg-white">
+      <div className="flex h-full">
+        {/* LEFT: vertical base sidebar */}
+        <BaseSidebar />
+
+        {/* RIGHT: everything else */}
+        <div className="flex min-w-0 flex-1 flex-col">
+          <AppTopBar />
+          <TableTabsBar />
+          <ViewActionBar />
+
+          {/* Main content area */}
+          <div className="flex-1 overflow-auto">
+            {children}
+          </div>
+        </div>
       </div>
     </div>
   );
